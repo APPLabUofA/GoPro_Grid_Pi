c77b7c715792bd61b53b2ba00af061b5bcb51120
Existentialist-Robot <eredman@ualberta.ca>
2019-05-07 14:54:07 -0600

    Reorganize

--- Visual_P3_GoPro_Grid.py
+++ Visual_P3_GoPro_Grid.py
@@ -6,35 +6,29 @@ import RPi.GPIO as GPIO
 import time
 from random import randint, shuffle
 import numpy as np
-import cv2
+from gpiozero import LED
+from gpiozero.pins.pigpio import PiGPIOFactory
 
-#Define Window
-screen_res = 1080,1920 
-width = int(screen_res[0]/2) # half width
-height = int(screen_res[1]/2) # half height
+# I approach pin organization by setting the remote pins to be native of the local machine and redefining the lcoal pins under a different 'factory'
+local_factory = PiGPIOFactory(host='192.168.1.3') # Server Pi - needs to be specified - since most is happening with the other Pi's pins
+#remote_factory = PiGPIOFactory(host='192.168.1.4') # Client Pi - don't specify here as long as you run the script, preceded by the following environmental variables: GPIOZERO_PIN_FACTORY=pigpio PIGPIO_ADDR=192.168.1.3
+led_1 = LED() # remote pins
+led_2 = LED(17, pin_factory=local_factory) ) # local pins
 
-#Define Instructions
-instr_0 = 'Press the space bar once to continue'
-instr_1 = 'During the duration of the experiment, focus on central fixation'
-instr_2 = 'Press the button when you see blue flashes'
-instr_3 = 'Do NOT press the button when you see green flashes.'
-instr_4 = 'When you are ready to start, press the space bar and THEN the button.'
-instr_5 = 'Feel free to take a break at this time.'
-instr_6 = 'To continue the experiment, press the space bar and THEN the button.'
-instr_7 = 'Congratulations, you have finished the experiment!'
-instr_8 = 'Please contact the experimenter.'
+# https://www.raspberrypi.org/magpi/make-neopixel-cosplay-eyes-with-pi/
+#https://gpiozero.readthedocs.io/en/stable/api_generic.html
+#gpiozero.DigitalOutputDevice
+#gpiozero.OutputDevice
+#gpiozero.PWMOutputDevice
+#This library uses Broadcom (BCM) pin numbering for the GPIO pins
 
-# Instruct lists for looping 
-Space_Propmt = [instr_0]
-Start_Instruct = [instr_0,instr_1, instr_2, instr_3, instr_4]
-Break_Instruct = [instr_5, instr_6]
-End_Instruct = [instr_7, instr_8]
-
-# Positions of text # Change all these - independent of aspect ratio
-Space_Propmt_Width = [(int(screen_res[1]/3),int(screen_res[1]/3))]
-Start_Instruct_Width = [(int(screen_res[1]/3),int(screen_res[1]/3)),(int(screen_res[1]/5),int(screen_res[1]/3)),(int(screen_res[1]/3),int(screen_res[1]/3)),(int(screen_res[1]/4),int(screen_res[1]/3)),(int(screen_res[1]/6),int(screen_res[1]/3)),(int(screen_res[1]/4),int(screen_res[1]/3))]
-Break_Instruct_Width = [(int(screen_res[1]/3),int(screen_res[1]/3)),(int(screen_res[1]/6),int(screen_res[1]/2))]
-End_Instruct_Width =  [(int(screen_res[1]/4),int(screen_res[1]/3)),(int(screen_res[1]/3),int(screen_res[1]/2))]
+while True:
+    led_1.on()
+    led_2.off()
+    time.sleep(1)
+    led_1.off()
+    led_2.on()
+    time.sleep(1)
 
 ##setup some constant variables##
 partnum = input("partnum: ")
@@ -69,9 +63,6 @@ resp_pin = 21
 
 ##amount of time needed to reset triggers##
 trig_gap = 0.005
-
-##define the ip address for the second Pi we will be controlling##
-##pi = pigpio.pi('192.168.1.216')
 
 ###initialise GPIO pins###
 GPIO.setmode(GPIO.BCM)
@@ -192,12 +183,9 @@ def rainbow_cycle(wait, rainbow_time):
             pixels.show()
             time.sleep(wait)
 
-def refresh_fixation(): 
-    img[int(width-width/100):int(width+width/100), int(height-height/20):int(height+height/20), :] = 255
-    img[int(width-width/20):int(width+width/20), int(height-height/100):int(height+height/100), :] = 255
-
 def refresh_trig_visor(x): # structure output of CSV
     trig_type.append(x)
+    trig_time.append(time.time() - start_exp)
     delay_length.append(2)
     trial_resp.append(0)
     resp_latency.append(0)
@@ -227,96 +215,60 @@ pixels = neopixel.NeoPixel(pin_out, pin_num, brightness = brightness, auto_write
 
 ##to significy the start of the experiment we make the LEDs all red initially and then wait for a certain amount of time##
 
-#Construct window + draw instructions before the experiment begins
-if __name__ == '__main__':
-    window_name = 'projector'
-    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-    cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-    for i in range(len(Start_Instruct)):
-        img = np.zeros((int(screen_res[0]),int(screen_res[1]),3), np.uint8) # 1960 X 1200 X 3 # all zeros = all black
-        cv2.putText(img,Start_Instruct[i],Start_Instruct_Width[i], cv2.FONT_HERSHEY_COMPLEX_SMALL , 1,(255,255,255),2,cv2.LINE_AA)
-        refresh_fixation()
-        cv2.imshow(window_name, img)
-        if cv2.waitKey(0) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            break
 ######## Actual Experiment Starts  ######
-    for block in range(block_num):
-        img = np.zeros((int(screen_res[0]),int(screen_res[1]),3), np.uint8) # 1960 X 1200 X 3 # all zeros = all black
-        refresh_fixation()
-        cv2.imshow(window_name, img)
-        cv2.waitKey(10)
-        GPIO.wait_for_edge(resp_pin,GPIO.RISING) ## Waits for an initial button press to turn on the LED (red)
-        pixels.fill(red)
-        GPIO.output(pi2trig(10),1) # send unique trigger for the start of the block
-        if block == 0:
-            start_exp = time.time()
-            exp_start_stop.append(0)
+for block in range(block_num):
+    GPIO.wait_for_edge(resp_pin,GPIO.RISING) ## Waits for an initial button press to turn on the LED (red)
+    pixels.fill(red)
+    GPIO.output(pi2trig(10),1) # send unique trigger for the start of the block
+    if block == 0:
+        start_exp = time.time()
+        exp_start_stop.append(0)
+    block_start_stop.append(time.time() - start_exp) # start of each block from start_exp
+    refresh_trig_visor(3)
+    time.sleep(2)
+    for i_trial in range(len(trials)):
+        start_trial = time.time() + trig_gap # define start time of a given trial
+        delay = ((randint(0,500)*0.001)+1.0) # define delay, to be used later
+        delay_length.append(delay)
+        ##determine the type of stimuli we will show on this trial##
+        if trials[i_trial] == 0: #standards
+            trig = 1
+            pixels.fill(green)
+    ##                pi.write(4, 1)
+        elif trials[i_trial] == 1: #targets
+            trig = 2
+            pixels.fill(blue)
+    ##                pi.write(17, 1)
+        GPIO.output(pi2trig(trig),1) ## Specify which trigger to send Standard vs Target
+        trig_type.append(trig)
         trig_time.append(time.time() - start_exp)
-        block_start_stop.append(time.time() - start_exp) # start of each block from start_exp
-        refresh_trig_visor(3)
-        time.sleep(2)
-        for i_trial in range(len(trials)):
-            start_trial = time.time() + trig_gap # define start time of a given trial
-            delay = ((randint(0,500)*0.001)+1.0) # define delay, to be used later
-            delay_length.append(delay)
-            ##determine the type of stimuli we will show on this trial##
-            if trials[i_trial] == 0: #standards
-                trig = 1
-                pixels.fill(green)
-        ##                pi.write(4, 1)
-            elif trials[i_trial] == 1: #targets
-                trig = 2
-                pixels.fill(blue)
-        ##                pi.write(17, 1)
-            GPIO.output(pi2trig(trig),1) ## Specify which trigger to send Standard vs Target
-            trig_type.append(trig)
-            trig_time.append(time.time() - start_exp)
-            time.sleep(trig_gap)
+        time.sleep(trig_gap)
 
-            GPIO.output(pi2trig(255),0)
-            resp_time = get_resp_led_off(resp_pin, 1.0,trig) # before_second_light, after_second_light
-            resp_time = get_resp(resp_pin, delay, 1.0, resp_time,trig)
-            resp_latency.append(time.time() - start_exp)
-            trial_resp.append(resp_time)
+        GPIO.output(pi2trig(255),0)
+        resp_time = get_resp_led_off(resp_pin, 1.0,trig) # before_second_light, after_second_light
+        resp_time = get_resp(resp_pin, delay, 1.0, resp_time,trig)
+        resp_latency.append(time.time() - start_exp)
+        trial_resp.append(resp_time)
 
-            GPIO.output(pi2trig(255),0) ## doesn't give us a trigger
-            time.sleep(trig_gap)
-            end_trial = time.time()
+        GPIO.output(pi2trig(255),0) ## doesn't give us a trigger
+        time.sleep(trig_gap)
+        end_trial = time.time()
 
 
-            trial_count += 1
-        ##end of block##
-        pixels.fill(red)
-        GPIO.output(pi2trig(11),1) # send unique trigger for the end of a block
-        trig_time.append(time.time() - start_exp)
-        block_start_stop.append(time.time() - start_exp) # end of each block from start_exp
-        refresh_trig_visor(4)
-        time.sleep(2)
-        # Break Screen Instructions
-        if trial_count < block_num:
-            img = np.zeros((int(screen_res[0]),int(screen_res[1]),3), np.uint8) # 1960 X 1200 X 3 # all zeros = all black
-            cv2.putText(img,Break_Instruct[0],Break_Instruct_Width[0], cv2.FONT_HERSHEY_COMPLEX_SMALL , 1,(255,255,255),2,cv2.LINE_AA)
-            cv2.putText(img,Break_Instruct[1],Break_Instruct_Width[1], cv2.FONT_HERSHEY_COMPLEX_SMALL , 1,(255,255,255),2,cv2.LINE_AA)
-            refresh_fixation()
-            cv2.imshow(window_name, img)
-            cv2.waitKey(0)
+        trial_count += 1
+    ##end of block##
+    pixels.fill(red)
+    GPIO.output(pi2trig(11),1) # send unique trigger for the end of a block
+    trig_time.append(time.time() - start_exp)
+    block_start_stop.append(time.time() - start_exp) # end of each block from start_exp
+    refresh_trig_visor(4)
+    time.sleep(2)
 
-# End of the experiment           
-exp_start_stop.append(time.time() - start_exp)
-img = np.zeros((int(screen_res[0]),int(screen_res[1]),3), np.uint8) # 1960 X 1200 X 3 # all zeros = all black
-cv2.putText(img,End_Instruct[0],End_Instruct_Width[0], cv2.FONT_HERSHEY_COMPLEX_SMALL , 1,(255,255,255),2,cv2.LINE_AA)
-cv2.putText(img,End_Instruct[1],End_Instruct_Width[1], cv2.FONT_HERSHEY_COMPLEX_SMALL , 1,(255,255,255),2,cv2.LINE_AA)
-cv2.imshow(window_name, img)
-cv2.waitKey(0)
 
 rainbow_cycle(0.001, 5) ## After all blocks flash a rainbow at a refresh of (1st arguement) ms for (2nd arguement) seconds
-
 pixels.fill(blank)
-cv2.destroyAllWindows()
 
 ###save trial information###
 filename_part = ("/home/pi/GitHub/GoPro_Visor_Eye_Pi/Pilot_Data/Experiment_1/" + partnum + "_" + filename + ".csv")
 
-
-np.savetxt(filename_part, (trig_type,trig_time, delay_length, trial_resp, resp_latency, block_start_stop, exp_start_stop), delimiter=',',fmt="%s")
+np.savetxt(filename_part, (trig_type,trig_time, delay_length, trial_resp, resp_latency, block_start_stop), delimiter=',',fmt="%s")
